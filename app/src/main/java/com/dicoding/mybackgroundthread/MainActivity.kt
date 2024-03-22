d1af059ebfa3806e90539c8de44f5afc37908480
avrians <avriansyahb10@gmail.com>
2024-03-22 09:46:36 +0700

    [av][add][masih terjadi freeze ketika aplikasi background berjalan]

--- app/src/main/java/com/dicoding/mybackgroundthread/MainActivity.kt
+++ app/src/main/java/com/dicoding/mybackgroundthread/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.dicoding.mybackgroundthread
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,6 +10,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        val btnStart = findViewById<Button>(R.id.btn_start)
+        val tvStatus = findViewById<TextView>(R.id.tv_status)
+        btnStart.setOnClickListener {
+            try {
+                //simulate process compressing
+                for (i in 0..10) {
+                    Thread.sleep(500)
+                    val percentage = i * 10
+                    if (percentage == 100) {
+                        tvStatus.setText(R.string.task_completed)
+                    } else {
+                        tvStatus.text = String.format(getString(R.string.compressing), percentage)
+                    }
+                }
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+        }
     }
 }
